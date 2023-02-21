74f0b4fe4df9c626d6db050fdfe154a836aa37c7
Joel Watkins <eck087@yahoo.co.uk>
2023-02-21 16:12:58 +0000

    set up express and get posts route

---
+++ server/server.js
@@ -0,0 +1,26 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+dotenv.config();
+import { PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient();
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/posts", async (req, res) => {
+  const posts = await prisma.post.findMany({
+    select: {
+      id: true,
+      title: true,
+    },
+  });
+
+  res.status(200).json({ posts });
+});
+
+const port = process.env.PORT || 4000;
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
