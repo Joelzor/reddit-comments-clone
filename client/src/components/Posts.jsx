15d01db6a4a77ea94c64f8643eeaf732e4a093e6
Joel Watkins <eck087@yahoo.co.uk>
2023-02-22 16:37:29 +0000

    post titles now displaying

--- client/src/components/Posts.jsx
+++ client/src/components/Posts.jsx
@@ -1,14 +1,24 @@
 import { React, useEffect, useState } from "react";
+// import { Link } from "react-router-dom";
 import getPosts from "../utils/posts";
 
 const Posts = () => {
   const [posts, setPosts] = useState([]);
 
   useEffect(() => {
-    getPosts().then(setPosts);
-  });
+    getPosts().then((data) => {
+      const { posts } = data;
+      setPosts(posts);
+    });
+  }, []);
 
-  return <div>{JSON.stringify(posts)}</div>;
+  return posts.map((post) => {
+    return (
+      <h1 key={post.id}>
+        <a href={`/posts/${post.id}`}>{post.title}</a>
+      </h1>
+    );
+  });
 };
 
 export default Posts;
