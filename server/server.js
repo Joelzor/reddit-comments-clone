3ee766b99129e8938bb3a62a80b5a52010825acb
Joel Watkins <eck087@yahoo.co.uk>
2023-02-23 10:37:11 +0000

    get post by id endpoint added

--- server/server.js
+++ server/server.js
@@ -27,6 +27,39 @@ app.get("/posts", async (req, res) => {
   res.status(200).json(posts);
 });
 
+app.get("/posts/:id", async (req, res) => {
+  const { id } = req.params;
+
+  const post = await prisma.post.findUnique({
+    where: {
+      id,
+    },
+    select: {
+      body: true,
+      title: true,
+      comments: {
+        orderBy: {
+          createdAt: "desc",
+        },
+        select: {
+          id: true,
+          message: true,
+          parentId: true,
+          createdAt: true,
+          user: {
+            select: {
+              id: true,
+              name: true,
+            },
+          },
+        },
+      },
+    },
+  });
+
+  res.status(200).json(post);
+});
+
 const port = process.env.PORT || 4000;
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
