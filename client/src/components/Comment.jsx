b19838af2a9ea98adf500416758bbd541c1662ac
Joel Watkins <eck087@yahoo.co.uk>
2023-02-23 15:01:49 +0000

    nested comments rendering & hide replies buttons working

--- client/src/components/Comment.jsx
+++ client/src/components/Comment.jsx
@@ -1,6 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 import IconBtn from "./IconBtn";
-import { FaHeart } from "react-icons/fa";
+import { FaHeart, FaReply, FaEdit, FaTrash } from "react-icons/fa";
+import { usePost } from "../contexts/PostContext";
+import Comments from "./Comments";
 
 const dateFormatter = new Intl.DateTimeFormat(undefined, {
   dateStyle: "medium",
@@ -8,6 +10,9 @@ const dateFormatter = new Intl.DateTimeFormat(undefined, {
 });
 
 const Comment = ({ id, message, user, createdAt }) => {
+  const { getReplies } = usePost();
+  const commentReplies = getReplies(id);
+  const [repliesHidden, setRepliesHidden] = useState(false);
   return (
     <>
       <div className="comment">
@@ -19,9 +24,36 @@ const Comment = ({ id, message, user, createdAt }) => {
         </div>
         <div className="message">{message}</div>
         <div className="footer">
-          <IconBtn Icon={FaHeart}>2</IconBtn>
+          <IconBtn Icon={FaHeart} aria-label="Like">
+            2
+          </IconBtn>
+          <IconBtn Icon={FaReply} aria-label="Reply" />
+          <IconBtn Icon={FaEdit} aria-label="Edit" />
+          <IconBtn Icon={FaTrash} aria-label="Delete" color="danger" />
         </div>
       </div>
+      {commentReplies?.length > 0 && (
+        <>
+          <div
+            className={`nested-comments-stack ${repliesHidden ? "hide" : ""}`}
+          >
+            <button
+              className="collapse-line"
+              aria-label="Hide Replies"
+              onClick={() => setRepliesHidden(true)}
+            />
+            <div className="nested-comments">
+              <Comments comments={commentReplies} />
+            </div>
+          </div>
+          <button
+            className={`btn mt-1 ${!repliesHidden ? "hide" : ""}`}
+            onClick={() => setRepliesHidden(false)}
+          >
+            Show replies
+          </button>
+        </>
+      )}
     </>
   );
 };
