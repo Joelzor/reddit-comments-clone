5fd5e7ea5596a1b0b250a2b0153b9831b2e79bde
Joel Watkins <eck087@yahoo.co.uk>
2023-02-24 15:03:49 +0000

    replying to comments implemented

--- client/src/components/Comment.jsx
+++ client/src/components/Comment.jsx
@@ -3,6 +3,9 @@ import IconBtn from "./IconBtn";
 import { FaHeart, FaReply, FaEdit, FaTrash } from "react-icons/fa";
 import { usePost } from "../contexts/PostContext";
 import Comments from "./Comments";
+import CreateComment from "./CreateComment";
+import { useAsyncFunction } from "../hooks/useAsync";
+import { createComment } from "../utils/comments";
 
 const dateFormatter = new Intl.DateTimeFormat(undefined, {
   dateStyle: "medium",
@@ -10,9 +13,25 @@ const dateFormatter = new Intl.DateTimeFormat(undefined, {
 });
 
 const Comment = ({ id, message, user, createdAt }) => {
-  const { getReplies } = usePost();
+  const {
+    error,
+    loading,
+    execute: commentReplyFn,
+  } = useAsyncFunction(createComment);
+  const { getReplies, createLocalComment, post } = usePost();
   const commentReplies = getReplies(id);
   const [repliesHidden, setRepliesHidden] = useState(false);
+  const [isReplying, setIsReplying] = useState(false);
+
+  const onCommentReply = (message) => {
+    return commentReplyFn({ postId: post.id, message, parentId: id }).then(
+      (comment) => {
+        createLocalComment(comment);
+        setIsReplying(false);
+      }
+    );
+  };
+
   return (
     <>
       <div className="comment">
@@ -27,11 +46,26 @@ const Comment = ({ id, message, user, createdAt }) => {
           <IconBtn Icon={FaHeart} aria-label="Like">
             2
           </IconBtn>
-          <IconBtn Icon={FaReply} aria-label="Reply" />
+          <IconBtn
+            isActive={isReplying}
+            Icon={FaReply}
+            aria-label={isReplying ? "Cancel Reply" : "Reply"}
+            onClick={() => setIsReplying(!isReplying)}
+          />
           <IconBtn Icon={FaEdit} aria-label="Edit" />
           <IconBtn Icon={FaTrash} aria-label="Delete" color="danger" />
         </div>
       </div>
+      {isReplying && (
+        <div className="mt-1 ml-3">
+          <CreateComment
+            autoFocus
+            onSubmit={onCommentReply}
+            loading={loading}
+            error={error}
+          />
+        </div>
+      )}
       {commentReplies?.length > 0 && (
         <>
           <div
