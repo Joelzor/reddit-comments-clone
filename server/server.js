d85c4518b8e69322f64b01d6005d7fa12dc82b7e
Joel Watkins <eck087@yahoo.co.uk>
2023-02-24 11:01:41 +0000

    setting up post route and added fake user login

--- server/server.js
+++ server/server.js
@@ -1,6 +1,8 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+
 dotenv.config();
 import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
@@ -15,6 +17,23 @@ app.use(
   })
 );
 app.use(express.json());
+app.use(cookieParser(process.env.COOKIE_SECRET));
+
+// cheat log in- always logged in as the same user
+const CURRENT_USER_ID = await prisma.user.findFirst({
+  where: {
+    name: "Kyle",
+  },
+}).id;
+
+app.use((req, res, next) => {
+  if (req.cookies.userId !== CURRENT_USER_ID) {
+    req.cookies.userId = CURRENT_USER_ID;
+    res.clearCookie("userId");
+    res.cookie("userId", CURRENT_USER_ID);
+  }
+  next();
+});
 
 app.get("/posts", async (req, res) => {
   const posts = await prisma.post.findMany({
@@ -59,6 +78,21 @@ app.get("/posts/:id", async (req, res) => {
   });
 
   res.status(200).json(post);
+});
+
+app.post("/posts/:id/comments", async (req, res) => {
+  if (req.body.message === "" || req.body.message === null) {
+    res.status(400).send("A comment cannot be empty");
+  }
+
+  await prisma.comment.create({
+    data: {
+      message: req.body.message,
+      userId: req.cookies.userId,
+      parentId: req.body.parentId,
+      postId: req.params.id,
+    },
+  });
 });
 
 const port = process.env.PORT || 4000;
